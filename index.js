35e59efd3bccdf992fe1369d177e62beb0dc82ab
Machigatta <iappware@googlemail.com>
2019-05-07 15:40:20 +0200

    init

---
+++ index.js
@@ -0,0 +1,75 @@
+var request = require("request")
+
+let Scraper = function () {
+    this.storeApiPattern = "https://store.playstation.com/store/api/chihiro/00_09_000/container/{REGION}/{LANGUAGE}/999/{OBJECT}";
+}
+
+let StoreObject = function (id, name, old_price, discountCampaign, oType, oPlatforms, oReleaseDate, oProvider, oDescription, oImages) {
+    this.id = id;
+    this.name = name;
+    this.old_price = old_price;
+    this.discount = discountCampaign;
+    this.type = oType;
+    this.platforms = oPlatforms;
+    this.release_date = oReleaseDate;
+    this.provider = oProvider;
+    this.description = oDescription;
+    this.images = oImages;
+}
+
+Scraper.prototype.scrape = async function (url, callback) {
+
+    var mRegExp = new RegExp(/(https:\/\/store.playstation.com\/)(.*)(\/.*\/)(.*)/);
+    if (!mRegExp.test(url)) {
+        throw "Nope";
+    } else {
+        let res = mRegExp.exec(url);
+        let product = res[4];
+        let language = res[2].split("-")[0];
+        let region = res[2].split("-")[1];
+
+        let storeApiUrl = this.storeApiPattern;
+        storeApiUrl = storeApiUrl.replace("{REGION}", region);
+        storeApiUrl = storeApiUrl.replace("{LANGUAGE}", language);
+        storeApiUrl = storeApiUrl.replace("{OBJECT}", product);
+
+
+        try {
+            console.log(storeApiUrl);
+
+            let jsonBody = JSON.parse(await getJSON(storeApiUrl));
+            let sObj = new StoreObject(
+                jsonBody.id,
+                jsonBody.name,
+                jsonBody.default_sku.display_price,
+                (jsonBody.default_sku.rewards != undefined) ? { new_price: jsonBody.default_sku.rewards[0].display_price, plus_required: jsonBody.default_sku.rewards[0].isPlus, discount_percent: jsonBody.default_sku.rewards[0].discount } : null,
+                jsonBody.game_contentType,
+                jsonBody.playable_platform,
+                jsonBody.release_date,
+                jsonBody.provider_name,
+                jsonBody.long_desc,
+                jsonBody.images
+            );
+            (typeof callback != 'undefined') ? callback(sObj) : null;
+            return sObj;
+        } catch (error) {
+            console.error('ERROR:');
+            console.error(error);
+        }
+
+    }
+}
+
+function getJSON(url) {
+    return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+            if (error) reject(error);
+            if (response.statusCode != 200) {
+                reject('Invalid status code <' + response.statusCode + '>');
+            }
+            resolve(body);
+        });
+    });
+}
+
+module.exports.Scraper = Scraper;
---
+++ readme.md
@@ -0,0 +1,53 @@
+# psn-store-scraper
+
+
+### Install
+
+Fetch the newest version via NPM
+
+`npm install psn-store-scraper`
+
+### Usage
+
+Import the constructor:
+
+```js 
+let Scraper = require('psn-store-scraper').Scraper;
+```
+
+Using the plain object retrieve:
+```js 
+let sObject = Scraper.scrape("https://store.playstation.com/de-de/product/EP1003-CUSA02231_00-DISHONOREDGAMEEU");
+```
+
+Calling a Callback:
+```js 
+Scraper.scrape("https://store.playstation.com/de-de/product/EP1003-CUSA02231_00-DISHONOREDGAMEEU", function(sObject){
+    console.log(sObject.name);
+});
+```
+
+
+### StoreObject
+
+```js
+    id
+    name
+    old_price
+    discount {
+        new_price
+        plus_required
+        discount_percent
+    }
+    type
+    platforms
+    release_date
+    provider
+    description
+    images [
+        {
+            type
+            url
+        }
+    ]
+```
